387127c4eaecdcef9b21e2f541b11da522d82fc4
vcosmin2701 <v.cosmin2717@gmail.com>
2026-07-16 23:04:00 +0300

    Single and multi-line comments

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,6 +1,15 @@
 fn main() {
+    // println! is called: macro
     println!("Hello, world!");
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
 }
 
 // Step to generate binary: rustc main.rs
 // Create folder with essentials: cargo new name
+
+/*
+ * Multi-line comments can be written like this
+ * Worth to mention: /* disable blocks of functionality inline */
+ */
